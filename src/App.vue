b79165ef8c41c666261c46f00787799c3208eff9
cds-mm <mykhaylo.markov@connecteddata.solutions>
2020-09-24 18:58:51 +0100

    Added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
 <!-- <body> -->
     <div>
-        <router-view></router-view>
+        <header>
+            <nav class="navbar">
+                <div class="navbar-menu">
+                    <div class="navbar-start">
+                        <router-link class="navbar-item" to="/">Home</router-link>
+                        <router-link class="navbar-item" to="/students">Students</router-link>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        
+        <main>
+            <div class="container">
+                <router-view></router-view>
+            </div>
+        </main>        
     </div>
 <!-- </body> -->
 </template>
